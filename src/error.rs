4753587708b9c5556519be78bc3700650cca0627
madosuki <yurikasamaaikatsu@gmail.com>
2023-06-12 16:59:57 +0900

    remove anyhow

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,4 @@
-use std::io::prelude::Write;
-
+use std::ffi::c_int;
 use libarchive3_sys_by_madosuki as libarchive3_sys;
 use thiserror::Error;
 
@@ -10,24 +9,40 @@ pub enum LibArchiveInternalStatus {
     ArchiveEof = libarchive3_sys::ARCHIVE_EOF as isize,
     ArchiveFatal = libarchive3_sys::ARCHIVE_FATAL as isize,
     ArchiveRetry = libarchive3_sys::ARCHIVE_RETRY as isize,
-    ArchiveWarn = libarchive3_sys::ARCHIVE_WARN as isize
+    ArchiveWarn = libarchive3_sys::ARCHIVE_WARN as isize,
+    Unknown,
 }
 
-impl std::fmt::Display for LibArchiveStatus {
+impl std::fmt::Display for LibArchiveInternalStatus {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let msg = generate_message(self);
         writeln!(f, "Status: {}", msg)
     }
 }
 
-fn generate_message(status: &LibArchiveStatus) -> String {
+impl std::convert::From<c_int> for LibArchiveInternalStatus {
+    fn from(v: c_int) -> Self {
+        match v {
+            libarchive3_sys::ARCHIVE_OK => Self::ArchiveOk,
+            libarchive3_sys::ARCHIVE_EOF => Self::ArchiveEof,
+            libarchive3_sys::ARCHIVE_WARN => Self::ArchiveWarn,
+            libarchive3_sys::ARCHIVE_FATAL => Self::ArchiveFatal,
+            libarchive3_sys::ARCHIVE_RETRY => Self::ArchiveRetry,
+            libarchive3_sys::ARCHIVE_FAILED => Self::ArchiveFailed,
+            _ => Self::Unknown,
+        }
+    }
+}
+
+fn generate_message(status: &LibArchiveInternalStatus) -> String {
     match status {
         LibArchiveInternalStatus::ArchiveOk => "Ok".to_owned(),
         LibArchiveInternalStatus::ArchiveFailed => "Failed".to_owned(),
         LibArchiveInternalStatus::ArchiveFatal => "Fatal".to_owned(),
         LibArchiveInternalStatus::ArchiveEof => "Eof".to_owned(),
         LibArchiveInternalStatus::ArchiveWarn => "Warn".to_owned(),
-        LibArchiveInternalStatus::ArchiveRetry => "Retry".to_owned()
+        LibArchiveInternalStatus::ArchiveRetry => "Retry".to_owned(),
+        _ => "Unknown".to_owned(),
     }
 }
 
@@ -35,6 +50,10 @@ fn generate_message(status: &LibArchiveStatus) -> String {
 pub enum LibArchiveError {
     #[error("Null")]
     Null,
+    #[error("NulError from ffi")]
+    NulError,
+    #[error("Failed get metadata from file")]
+    FailedGetMetaDataFromFile,
     #[error("is not file")]
     IsNotFile,
     #[error("is not exists")]
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use libarchive3_sys_by_madosuki as libarchive3_sys;
-use libarchive3_sys::ArchiveError;
 
 mod error;
 use error::{LibArchiveError, LibArchiveResult, LibArchiveInternalStatus};
@@ -10,7 +9,7 @@ pub struct Archive {
 
 pub trait ArchiveExt {
     fn new() -> LibArchiveResult<Archive>;
-    fn load_compressed_file(&self, file: &str) -> anyhow::Result<()>;
+    fn load_compressed_file(&self, file: &str) -> LibArchiveResult<()>;
 }
 
 impl ArchiveExt for Archive {
@@ -24,15 +23,26 @@ impl ArchiveExt for Archive {
     }
 
     fn load_compressed_file(&self, file_path: &str) -> LibArchiveResult<()> {
-        let _meta = std::fs::metadata(file_path).unwrap();
+        let Ok(_meta) = std::fs::metadata(file_path) else {
+            return Err(LibArchiveError::FailedGetMetaDataFromFile);
+        };
+        
         if !_meta.is_file() {
-            return Err(LibArchiveError::NotFile);
+            return Err(LibArchiveError::IsNotFile);
         }
 
-        let _file_path_cstr = std::ffi::CString::new(file_path).unwrap();
+        let Ok(_file_path_cstr) = std::ffi::CString::new(file_path) else {
+            return Err(LibArchiveError::NulError);
+        };
+
+        let _f_size = _meta.len() as usize;
         
-        let r = unsafe {
-            // libarchive3_sys::archive_read_open_filename(self.archive, &file_path.as_ptr(), 4737162)
+        unsafe {
+            let _status_code = libarchive3_sys::archive_read_open_filename(self.archive, _file_path_cstr.as_ptr(), _f_size);
+            if _status_code != 0 {
+                let errno = libarchive3_sys::archive_errno(self.archive);
+                return Err(LibArchiveError::LibArchiveInternalError(LibArchiveInternalStatus::from(errno)));
+            }
         };
 
         // let mut entry_count = 0;
